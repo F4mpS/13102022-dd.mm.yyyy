d22f1dbe5f8d2de294f24953068b9f6f2e1b955a
Alexander <sany2k13@mail.ru>
2022-11-09 22:22:43 +0300

    Дз за Семинар 4

---
+++ 07112022/Dz/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/*
+Console.Write("Enter ur number >>> ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter ur number >>> ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int start = a;
+
+for (int i = 1; i < b; i++)
+{
+    a = start * a;
+}
+
+Console.WriteLine(a);
+*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+/*
+Console.Write("Enter ur number >>> ");
+string num = Console.ReadLine();
+char[] symb_arr = num.ToCharArray();
+int sum = 0;
+
+foreach (char i in symb_arr)
+{
+    sum += i - '0';     // ascii творит чудеса
+}
+Console.WriteLine(sum);
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+/*
+int[] arr = new int[8];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100);
+}
+
+foreach(int i in arr)
+{
+    Console.WriteLine(i);
+}
+*/
+
+
+
---
+++ 07112022/Seminar_4/Program.cs
@@ -0,0 +1,78 @@
+﻿// Console.Write("Enter number >>> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(SumNum(num));
+
+// int SumNum(int number) 
+// {
+//     int sum = 0;
+//     int count = 0;
+
+//     while (number > count)
+//     {
+//         count++;
+//         sum += count;
+//     }
+
+//     return sum;
+// }
+
+
+/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+// Console.Write("Enter your number >>> ");
+// string num = Console.ReadLine();
+
+// char[] NumSymbols = num.ToCharArray(); 
+
+// Console.WriteLine(NumSymbols.Length);
+
+
+/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+// int size = 8;
+// int[] array = GetArray(size);
+
+// int[] GetArray(int size)
+// {
+//     int[] array = new int [size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(0, 2);
+//     }
+
+//     return array;
+// }
+
+// Console.WriteLine(String.Join(",", array));
+
+
+/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120
+*/
+
+Console.Write("Enter your number >>> ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Factorial(num));
+
+int Factorial(int num)
+{
+    int result = 1;
+    for (int i = 2; i < num + 1; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
