cc85faae73674af9b72e2c2b820939bde3040c19
F4mpS <sany2k13@mail.ru>
2022-11-21 19:14:09 +0300

    Дз за Семинар 7

---
+++ 11172022/Dz/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [row, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    { 
+        double randomReal = (new Random().NextDouble() * 10) * (new Random().Next(-1, 2)); // Генерация случайного вещественного числа от -9.9 до 10
+        matrix[i,j] = Math.Round(randomReal, 1);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+*/
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [row, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    { 
+        matrix[i,j] = new Random().Next(1, 10);
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter element position >>> ");
+int position = Convert.ToInt32(Console.ReadLine());
+int pos_x, pos_y;
+pos_x = position / 10 - 1;  // 0 элемент массива
+pos_y = position % 10 - 1;  // 6 элемент массива
+
+if (pos_y > columns|| pos_x > row){    // Проверка нахождения числа в масиве
+    Console.WriteLine("Sorry array does not have this element");
+} else{
+    Console.WriteLine($"{position} contains >>> {matrix[pos_x, pos_y]}");
+}
+*/
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double [row, columns];
+double [] arithmeticMean = new double [columns]; // Ср. арефм. столбцов
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    double sumOfColumnElements = 0; // Сумма элементов столбца
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1, 10);
+        Console.Write(matrix[i,j] + "\t");
+        sumOfColumnElements += matrix[i,j];
+    }
+    Console.WriteLine();
+    arithmeticMean[i] = sumOfColumnElements / columns;
+}
+Console.WriteLine("Averages in columns: " + string.Join(", ", arithmeticMean));
+*/
---
+++ 11172022/Seminar_7/Program.cs
@@ -0,0 +1,113 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [row, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(11);
+
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+*/
+
+// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [row, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = i + j;
+
+        Console.Write(matrix[i,j] + "\t");
+    }
+    for(int k = 0; k < 2; k++){
+        Console.WriteLine("\n");
+    }
+}
+*/
+
+
+// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+/*
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [row, columns];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(5,7);
+
+        if (i % 2 == 0 && j % 2 == 0) matrix[i, j] *= matrix[i,j]; 
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+*/
+
+
+// **Задача 51:** Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Write("Enter count of row >>> ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter count of columns >>> ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [row, columns];
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(11);
+
+        if (i == j) sum += matrix[i,j]; 
+        Console.Write(matrix[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("\t >>> " + sum);
+
+
+
+
